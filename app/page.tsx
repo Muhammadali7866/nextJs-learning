b0133a3a998e3ef97e9746a3e1d1fa800b09a820
Muhammadali7866 <malikali8157@gmail.com>
2024-11-02 14:35:57 +0500

    19-Link componenet navigation

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,15 @@
+import Link from "next/link";
+
 export default function Home() {
-  return <h1>Welcome to reloRadar</h1>;
+  return (
+    <>
+      <h1>Welcome to reloRadar</h1>
+      <h2>
+        <Link href="/blog">Blog</Link>
+      </h2>
+      <h2>
+        <Link href="/products">products</Link>
+      </h2>
+    </>
+  );
 }
--- app/products/page.tsx
+++ app/products/page.tsx
@@ -1,3 +1,23 @@
+import Link from "next/link";
+
 export default function Products() {
-  return <h1>welcome to products page</h1>;
+  return (
+    <>
+      <h1>welcome to products page</h1>
+      <h2>
+        <Link href="/products/1">Products 1</Link>
+      </h2>
+      <h2>
+        <Link href="/products/2">Products 2</Link>
+      </h2>
+      <h2>
+        <Link href="/products/3" replace>
+          Products 3
+        </Link>
+      </h2>
+      <h2>
+        <Link href="/">Home</Link>
+      </h2>
+    </>
+  );
 }
