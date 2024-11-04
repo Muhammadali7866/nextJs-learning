aa24c1067150d0f6db14ebc44ff71a02e3f81d36
Muhammadali7866 <malikali8157@gmail.com>
2024-11-04 16:40:51 +0500

    27-error handle in layout

--- app/products/[productId]/layout.tsx
+++ app/products/[productId]/layout.tsx
@@ -1,8 +1,15 @@
+function getRandom(count: number) {
+  return Math.floor(Math.random() * count);
+}
 export default function ProductDetailsLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
+  const random = getRandom(2);
+  if (random == 1) {
+    throw new Error("Error Loading product");
+  }
   return (
     <>
       {children}
