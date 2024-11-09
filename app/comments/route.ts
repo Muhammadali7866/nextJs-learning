c0d66f082953c84d6420c2ac85c8ee778f751dc9
Muhammadali7866 <malikali8157@gmail.com>
2024-11-09 22:49:19 +0500

    35-handling Post reequest

--- app/comments/route.ts
+++ app/comments/route.ts
@@ -3,3 +3,16 @@ import { comments } from "./data";
 export async function GET() {
   return Response.json(comments);
 }
+
+export async function POST(request: Request) {
+  const comment = await request.json();
+  let newComent = {
+    id: comments.length + 1,
+    text: comment.text,
+  };
+  comments.push(newComent);
+  return new Response(JSON.stringify(newComent), {
+    headers: { "content-type": "application/json" },
+    status: 201,
+  });
+}
