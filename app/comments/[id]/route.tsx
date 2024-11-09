ecb9e9dc49420b5702eca8c9de80b61b0c7ef4db
Muhammadali7866 <malikali8157@gmail.com>
2024-11-09 23:17:22 +0500

    37-handling path request

--- app/comments/[id]/route.tsx
+++ app/comments/[id]/route.tsx
@@ -13,3 +13,19 @@ export async function GET(
   const comment = comments.find((item) => item.id === parseInt(params.id));
   return Response.json(comment);
 }
+
+export async function PATCH(
+  request: Request,
+  {
+    params,
+  }: {
+    params: {
+      id: string;
+    };
+  }
+) {
+  const { text } = await request.json();
+  const index = comments.findIndex((_item) => _item.id === parseInt(params.id));
+  comments[index].text = text;
+  return Response.json(comments[index]);
+}
