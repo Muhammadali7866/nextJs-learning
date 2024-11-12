f0cbf64c1e7f6e31f457453a48f1c5e24788e47e
Muhammadali7866 <malikali8157@gmail.com>
2024-11-12 18:55:48 +0500

    catch all segments

---
+++ my-app/app/not-found.tsx
@@ -0,0 +1,8 @@
+export default function NotFound() {
+  return (
+    <>
+      <h1>Page not found</h1>
+      <h3>Requested resources not found</h3>
+    </>
+  );
+}
--- my-app/app/products/[productId]/reviews/[reviewsId]/page.tsx
+++ my-app/app/products/[productId]/reviews/[reviewsId]/page.tsx
@@ -1,3 +1,5 @@
+import { notFound } from "next/navigation";
+
 export default function ProductReview({
   params,
 }: {
@@ -6,6 +8,9 @@ export default function ProductReview({
     reviewsId: string;
   };
 }) {
+  if (parseInt(params.reviewsId) > 1000) {
+    return notFound();
+  }
   return (
     <>
       Review {params.reviewsId} for product {params.productId}
