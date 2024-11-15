0e7d48fe007bdc3ecf572b28565542c76ad1064f
Muhammadali7866 <malikali8157@gmail.com>
2024-11-15 17:10:21 +0500

    19-Link component navigation

--- my-app/app/products/page.tsx
+++ my-app/app/products/page.tsx
@@ -1,10 +1,23 @@
+import Link from "next/link";
+
 export default function Products() {
   return (
     <>
+      <h1>
+        <Link href="/">Home</Link>
+      </h1>
       <h2>Product Details</h2>
-      <h4>Product 1</h4>
-      <h4> product 2</h4>
-      <h4>product 2</h4>
+      <h4>
+        {" "}
+        <Link href="/products/1">Product 1</Link>
+      </h4>
+      <h4>
+        {" "}
+        <Link href="/products/2">Product 2</Link>
+      </h4>
+      <h4>
+        <Link href="/products/3">Product 3</Link>
+      </h4>
     </>
   );
 }
